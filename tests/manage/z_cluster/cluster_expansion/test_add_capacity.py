1005d2e15014c24670d853599b9b0389c4f5976c
Shreekar <sshreeka@redhat.com>
2020-03-03 21:01:55 +0530

    tests/manage/z_cluster/cluster_expansion/test_add_capacity.py  - Removed parametes for test function  - Added code for getting size of existing osd size
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/z_cluster/cluster_expansion/test_add_capacity.py
+++ tests/manage/z_cluster/cluster_expansion/test_add_capacity.py
@@ -2,7 +2,6 @@ import logging
 import pytest
 
 from ocs_ci.framework.testlib import ignore_leftovers, ManageTest, tier1
-from ocs_ci.framework.pytest_customization.marks import polarion_id, tier3
 from ocs_ci.ocs.resources import storage_cluster
 
 logger = logging.getLogger(__name__)
@@ -14,20 +13,10 @@ class TestAddCapacity(ManageTest):
     """
     Automates adding variable capacity to the cluster while IOs running
     """
-    @pytest.mark.parametrize(
-        argnames=[
-            "capacity"
-        ],
-        argvalues=[
-            pytest.param(*[1], marks=[polarion_id('OCS-1191'), tier3]),
-        ]
-    )
-    def test_add_capacity(self, capacity):
+    def test_add_capacity(self):
         """
         Test to add variable capacity to the OSD cluster while IOs running
-
-        Args:
-           capacity (int):the storage capacity to add as deviceSet number
         """
-        result = storage_cluster.add_capacity(capacity)
+        osd_size = storage_cluster.get_osd_size()
+        result = storage_cluster.add_capacity(osd_size)
         assert result, logger.info("Test Failed, new pods failed reaching running state")
